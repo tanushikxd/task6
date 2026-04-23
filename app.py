9833507ff4e7e2001cde97e97a7ad8958c2b132d
tanushikxd <tanushikxdt2012@gmail.com>
2026-04-23 03:17:14 +0100

    changed connections to db

--- app.py
+++ app.py
@@ -45,11 +45,12 @@ app = Flask(__name__)
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        port=int(os.getenv("DB_PORT", 3306)),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", ""),
-        database=os.getenv("DB_NAME", "fake_data")
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        ssl_disabled=False
     )
 
 @app.route("/", methods=["GET", "POST"])
